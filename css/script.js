5e7fea063bc6cf1c4e19abf9c465146ec603a1af
yokola666 <jocke.ekstrand@gmail.com>
2019-11-12 22:35:49 +0100

    latest changes

---
+++ css/script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$(".addClass").click(function (e) {
+		e.preventDefault();
+		$('#qnimate').addClass('popup-box-on');
+	});          
+	$("#removeClass").click(function () {
+		$('#qnimate').removeClass('popup-box-on');
+	});
+});
